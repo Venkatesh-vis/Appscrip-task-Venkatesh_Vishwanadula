2dd4bff4cdfe441e12b6dd19c5b90ada15cea571
Venkatesh-vis <venkateshvishwanadula257@gmail.com>
2024-08-20 11:58:00 +0530

    .

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./main.css";
 
 const Main = () => {
+  const [filterVisible, setFilterVisible] = useState(true);
+
+  const toggleFilter = () => {
+    setFilterVisible(!filterVisible);
+  };
   return (
     <>
       <div className="main">
@@ -8,52 +14,56 @@ const Main = () => {
           <div>
             <h5>3425 ITEMS</h5>
           </div>
-          <div>
+          <div onClick={toggleFilter} style={{ cursor: "pointer" }}>
             <img src="./arrow-left.png" alt="arrow" height={12} />
-            <p style={{ display: "inline-block" }}>HIDE FILTER</p>
+            <p style={{ display: "inline-block" }}>
+              {filterVisible ? "HIDE FILTER" : "SHOW FILTER"}
+            </p>
           </div>
           <div>
             <h5>RECOMMENDED</h5>
           </div>
         </div>
         <div className="mm">
-          <div className="mhead">
-            <div className="categories">
-              <h6>CUSTOMIZABLE</h6>
+          {filterVisible && (
+            <div className="mhead">
+              <div className="categories">
+                <h6>CUSTOMIZABLE</h6>
+              </div>
+              <div className="cat">
+                <h6>IDEAL FOR</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>OCCASION</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>WORK</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>FABRIC</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>SEGMENT</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>SUITABLE FOR</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>RAW MATERIALS</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
+              <div className="cat">
+                <h6>PATTERN</h6>
+                <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
+              </div>
             </div>
-            <div className="cat">
-              <h6>IDEAL FOR</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>OCCASION</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>WORK</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>FABRIC</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>SEGMENT</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>SUITABLE FOR</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>RAW MATERIALS</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-            <div className="cat">
-              <h6>PATTERN</h6>
-              <p style={{ marginTop: "-20px", fontSize: "10px" }}>ALL</p>
-            </div>
-          </div>
+          )}
           <div className="items">
             <div className="item">
               <img
